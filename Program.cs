9488e4f7cf046c1a7d2a25b5f5bd57b9c05af2db
Ermek Djumataev <ermekd@Ermeks-MacBook-Pro.local>
2020-06-07 17:36:09 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingProj
+{
+    class Program
+    {
+
+        static List<object> emptylist()
+        {
+            return new List<object>()
+            {
+                7,
+                28,
+                -1,
+                true,
+                "chair"
+            };
+        }
+        static void ObjList(List<object> words)
+        {
+            int num = 0;
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (words[i] is int)
+                {
+                    num += (int) words[i];
+                }
+            }
+            Console.WriteLine($"the sum is:\n{num}");
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            ObjList( emptylist() );
+        }
+    }
+}
